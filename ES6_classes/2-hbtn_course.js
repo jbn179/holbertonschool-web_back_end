b9b80affa94bbca9cd0b063a054826d850d9233d
jbn179 <9772@holbertonstudents.com>
2025-03-24 21:24:59 +0100

    fix task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -35,11 +35,11 @@ export default class HolbertonCourse {
     if (!Array.isArray(value)) {
       throw new TypeError('Students must be an array');
     }
-    
+
     if (!value.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    
+
     this._students = value;
   }
 }
