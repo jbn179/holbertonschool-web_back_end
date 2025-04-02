5abb71d68a67dc3168df883c14008098b0d1ae53
jbn179 <9772@holbertonstudents.com>
2025-04-02 14:42:56 +0200

    add task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+
+  const filteredValues = [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filteredValues.join('-');
+}
+
+export default cleanSet;
