56f217996d9502c2b147f1be3caf77cbce23b5a0
jbn179 <9772@holbertonstudents.com>
2025-04-02 14:33:23 +0200

    fix task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,8 @@
-function updateStudentGradeByCity(studentsList, city, newGrades) {
-  return studentsList
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
     .filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      
       return {
         ...student,
         grade: gradeObj ? gradeObj.grade : 'N/A',
