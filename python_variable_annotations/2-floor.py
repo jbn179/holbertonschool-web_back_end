347b96e8f5b40131ea0feaa43d860ed11b0876dd
jbn179 <9772@holbertonstudents.com>
2025-04-22 12:22:13 +0200

    add task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that provides a function for finding the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float.
+
+    Args:
+        n: float to be floored
+
+    Returns:
+        The floor of the float as an integer
+    """
+    return int(n)
