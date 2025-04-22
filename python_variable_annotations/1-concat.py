0b20f3f6219263f03b4c1a3943cd0008335c3cb7
jbn179 <9772@holbertonstudents.com>
2025-04-22 12:14:23 +0200

    add task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that provides a function for concatenating strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result.
+
+    Args:
+        str1: first string
+        str2: second string
+
+    Returns:
+        The concatenated string
+    """
+    return str1 + str2
