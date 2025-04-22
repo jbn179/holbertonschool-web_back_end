82f2fce9a0fe77118e03e93b68db149f69502993
jbn179 <9772@holbertonstudents.com>
2025-04-22 11:06:25 +0200

    add task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""Module that provides a function for adding two floats."""
+
 
 def add(a: float, b: float) -> float:
+    """Add two floating point numbers and return their sum.
+
+    Args:
+        a: first float
+        b: second float
+
+    Returns:
+        The sum of a and b
+    """
     return a + b
